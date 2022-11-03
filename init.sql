2463ef4ff1df10910b9c5c1de13a57701ee3f717
Khai Dong <dongk@union.edu>
2022-11-03 19:30:52 -0400

    add some data

--- init.sql
+++ init.sql
@@ -10,16 +10,25 @@ DROP TABLE IF EXISTS
 	building;
 
 -- re-initialize the tables
+
+CREATE TABLE IF NOT EXISTS parking_lot(
+	parking_lot_no INTEGER NOT NULL,
+    capacity INTEGER NOT NULL,
+    
+    PRIMARY KEY(parking_lot_no)
+);
+
 CREATE TABLE IF NOT EXISTS building(
-	building_number INTEGER,
+	building_no INTEGER,
     build_date DATE NOT NULL,
-    purchased_date DATE NOT NULL,
-    number_of_floor INTEGER NOT NULL,
-    number_of_room INTEGER NOT NULL,
+    no_of_floor INTEGER NOT NULL,
+    no_of_room INTEGER NOT NULL,
     allow_pets BINARY NOT NULL,
     address VARCHAR(500) NOT NULL,
+    parking_lot_no INTEGER NOT NULL,
     
-    PRIMARY KEY(building_number)
+    PRIMARY KEY(building_no),
+    FOREIGN KEY (parking_lot_no) REFERENCES parking_lot(parking_lot_no)
 );
 
 CREATE TABLE IF NOT EXISTS employee(
@@ -47,28 +56,21 @@ CREATE TABLE IF NOT EXISTS employee_phone(
 
 CREATE TABLE IF NOT EXISTS manager(
 	employee_ssn  CHAR(9) NOT NULL,
-    building_number INTEGER NOT NULL,
+    building_no INTEGER NOT NULL,
     
     PRIMARY KEY(employee_ssn),
     FOREIGN KEY(employee_ssn) REFERENCES employee(ssn),
-    FOREIGN KEY(building_number) REFERENCES building(building_number)
+    FOREIGN KEY(building_no) REFERENCES building(building_no)
 );
 
 CREATE TABLE IF NOT EXISTS worker(
 	employee_ssn  CHAR(9) NOT NULL,
-    building_number INTEGER NOT NULL,
+    building_no INTEGER NOT NULL,
     type VARCHAR(20), 
     
     PRIMARY KEY(employee_ssn),
     FOREIGN KEY(employee_ssn) REFERENCES employee(ssn),
-    FOREIGN KEY(building_number) REFERENCES building(building_number)
-);
-
-CREATE TABLE IF NOT EXISTS parking_lot(
-	parking_lot_number INTEGER NOT NULL,
-    capacity INTEGER NOT NULL,
-    
-    PRIMARY KEY(parking_lot_number)
+    FOREIGN KEY(building_no) REFERENCES building(building_no)
 );
 
 CREATE TABLE IF NOT EXISTS person(
@@ -120,38 +122,38 @@ CREATE TABLE IF NOT EXISTS tenant_vehicle(
 );
 
 CREATE TABLE IF NOT EXISTS parking_slot(
-	parking_lot_number INTEGER NOT NULL,
+	parking_lot_no INTEGER NOT NULL,
     section VARCHAR(5) NOT NULL,
-    slot_number INTEGER NOT NULL,
+    slot_no INTEGER NOT NULL,
     type VARCHAR(20) NOT NULL,
     
     vehicle_license_plate VARCHAR(10) NOT NULL,
     in_date DATE NOT NULL,
     in_time time NOT NULL,
 	
-    PRIMARY KEY(parking_lot_number, section, slot_number),
-    FOREIGN KEY(parking_lot_number) REFERENCES parking_lot(parking_lot_number),
+    PRIMARY KEY(parking_lot_no, section, slot_no),
+    FOREIGN KEY(parking_lot_no) REFERENCES parking_lot(parking_lot_no),
     FOREIGN KEY(vehicle_license_plate) REFERENCES vehicle(license_plate)
 );
 
 CREATE TABLE IF NOT EXISTS service(
-	building_number INTEGER NOT NULL,
+	building_no INTEGER NOT NULL,
 	name VARCHAR(20) NOT NULL,
     opentime time NOT NULL,
     closetime time NOT NULL,
     type VARCHAR(20) NOT NULL,
     operating_company VARCHAR(20) NOT NULL,
     
-    PRIMARY KEY(building_number, name),
-    FOREIGN KEY(building_number) REFERENCES building(building_number)
+    PRIMARY KEY(building_no, name),
+    FOREIGN KEY(building_no) REFERENCES building(building_no)
 );
 
 CREATE TABLE IF NOT EXISTS apartment(
-	building_number INTEGER NOT NULL, 
+	building_no INTEGER NOT NULL, 
     floor INTEGER NOT NULL, 
-    room_number INTEGER NOT NULL, 
-    number_of_bedroom INTEGER NOT NULL, 
-    number_of_bathroom INTEGER NOT NULL, 
+    room_no INTEGER NOT NULL, 
+    no_of_bedroom INTEGER NOT NULL, 
+    no_of_bathroom INTEGER NOT NULL, 
     type VARCHAR(20) NOT NULL, 
     size float NOT NULL,  
     lease_start_date DATE NOT NULL, 
@@ -161,26 +163,55 @@ CREATE TABLE IF NOT EXISTS apartment(
     monthly_rate float NOT NULL, 
     lease_tenant_phone VARCHAR(20) NOT NULL,
     
-    PRIMARY KEY(building_number, floor, room_number),
-	FOREIGN KEY(building_number) REFERENCES building(building_number),
+    PRIMARY KEY(building_no, floor, room_no),
+	FOREIGN KEY(building_no) REFERENCES building(building_no),
     FOREIGN KEY(lease_tenant_phone) REFERENCES tenant(person_phone)
 );
 
 CREATE TABLE IF NOT EXISTS subscribed(
 	person_phone VARCHAR(20) NOT NULL,
-	service_building_number INTEGER NOT NULL,
+	service_building_no INTEGER NOT NULL,
     service_name VARCHAR(20) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     type VARCHAR(20),
     
-    PRIMARY KEY(person_phone, service_building_number, service_name),
+    PRIMARY KEY(person_phone, service_building_no, service_name),
     FOREIGN KEY(person_phone) REFERENCES person(phone),
-    FOREIGN KEY(service_building_number, service_name) REFERENCES service(building_number, name)    
+    FOREIGN KEY(service_building_no, service_name) REFERENCES service(building_no, name)    
 );
 
 -- data population
 
+INSERT INTO parking_lot(parking_lot_no, capacity)
+VALUES
+	(1, 200),
+    (2, 300),
+    (3, 50),
+    (4, 70),
+    (5, 100);
 
+INSERT INTO building(building_no, build_date, no_of_floor, no_of_room, allow_pets, address, parking_lot_no)
+VALUES
+	(1, '2001-06-09', 2, 4, 1, "123 1st Street, Schenectady, NY, 12308-01", 1),
+	(2, '2001-07-09', 3, 6, 1, "124 1st Street, Schenectady, NY, 12309-02", 2),
+	(3, '2001-08-09', 2, 4, 1, "125 1st Street, Schenectady, NY, 12308-03", 3),
+	(4, '2001-09-09', 1, 2, 1, "126 1st Street, Schenectady, NY, 12308-04", 4),
+	(5, '2001-10-09', 5, 5, 1, "127 1st Street, Schenectady, NY, 12308-05", 5);
+
+INSERT INTO employee(ssn, fname, lname, mis, sex, email, dob, hourly_rate, hire_date, address)
+VALUES
+	("680824963", "Ronald", "Power", "A", "Male", "powerr@land.co", "1990-10-10", 30, "2010-08-10", "123 2nd Street, Schenectady, NY, 12309-01"),
+	("416155508", "Camille", "Hathaway", "D", "Female", "hathac@land.co", "1991-09-10", 20, "2010-08-10", "124 2nd Street, Schenectady, NY, 12309-02"),
+    ("232105522", "Justina", "Sergeant", "B", "Female", "sergej@land.co", "1980-01-15", 50, "2007-01-10", "125 2nd Street, Schenectady, NY, 12309-01"),
+    ("627386755", "Don", "Blue", "MV", "Male", "blued@land.co", "2000-05-15", 30, "2018-01-15", "126 2nd Street, Schenectady, NY, 12309-02"),
+    ("041865556", "Hung", "Trinh", "T", "Male", "trinhh@land.co", "2000-01-07", 10, "2018-08-10", "127 2nd Street, Schenectady, NY, 12309-03"),
+    ("502823428", "Candace", "Mould", "LD", "Female", "mouldc@land.co", "1995-07-10", 25, "2005-09-10", "128 2nd Street, Schenectady, NY, 12309-04"),
+    ("269019796", "Anastasia", "Marina", "MM", "Female", "marina@land.co", "1980-06-01", 60, "2000-08-10", "129 2nd Street, Schenectady, NY, 12309-05"),
+    ("469232069", "Peyton", "Davies", "J", "Female", "daviep@land.co", "1998-06-07", 25, "2012-08-07", "130 2nd Street, Schenectady, NY, 12309-06"),
+    ("545927795", "Toby", "Thompsett", "A", "Male", "thompst@land.co", "1997-10-10", 20, "2010-08-10", "131 2nd Street, Schenectady, NY, 12309-07"),
+    ("262117281", "Jade", "Proudfoot", "M", "Male", "prounj@land.co", "1992-07-10", 40, "2012-08-10", "132 2nd Street, Schenectady, NY, 12309-08"),
+    ("519707770", "Kara", "Oliverson", "IA", "Female", "olivek@land.co", "1980-01-10", 55, "1997-08-10", "133 2nd Street, Schenectady, NY, 12309-09"),
+    ("417605992","Minerva", "Leon", "A", "Female", "leonm@land.co", "2001-10-10", 15, "2021-08-06", "134 3rd Street, Schenectady, NY, 12330");
 COMMIT;
 
