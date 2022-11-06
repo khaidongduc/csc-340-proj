abfa64915a248b2d47a4ffc9f48ef23a22f2bf98
Khai Dong <dongk@union.edu>
2022-11-06 16:59:36 -0500

    beautify query

--- sql/init.sql
+++ sql/init.sql
@@ -12,28 +12,27 @@ DROP TABLE IF EXISTS
 
 -- re-initialize the tables
 
-CREATE TABLE IF NOT EXISTS parking_lot(
-	parking_lot_no INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS parking_lot (
+    parking_lot_no INTEGER NOT NULL,
     capacity INTEGER NOT NULL,
-    
-    PRIMARY KEY(parking_lot_no)
+    PRIMARY KEY (parking_lot_no)
 );
 
-CREATE TABLE IF NOT EXISTS building(
-	building_no INTEGER,
+CREATE TABLE IF NOT EXISTS building (
+    building_no INTEGER,
     build_date DATE NOT NULL,
     no_of_floor INTEGER NOT NULL,
     no_of_room INTEGER NOT NULL,
     allow_pets BINARY NOT NULL,
     address VARCHAR(500) NOT NULL,
     parking_lot_no INTEGER NOT NULL,
-    
-    PRIMARY KEY(building_no),
-    FOREIGN KEY (parking_lot_no) REFERENCES parking_lot(parking_lot_no)
+    PRIMARY KEY (building_no),
+    FOREIGN KEY (parking_lot_no)
+        REFERENCES parking_lot (parking_lot_no)
 );
 
-CREATE TABLE IF NOT EXISTS employee(
-	ssn VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS employee (
+    ssn VARCHAR(20) NOT NULL,
     fname VARCHAR(20) NOT NULL,
     lname VARCHAR(20) NOT NULL,
     mis VARCHAR(10) NOT NULL,
@@ -42,153 +41,156 @@ CREATE TABLE IF NOT EXISTS employee(
     dob DATE NOT NULL,
     hourly_rate FLOAT NOT NULL,
     hire_date DATE NOT NULL,
-	address VARCHAR(500) NOT NULL,
-    
-    PRIMARY KEY(ssn)
+    address VARCHAR(500) NOT NULL,
+    PRIMARY KEY (ssn)
 );
 
-CREATE TABLE IF NOT EXISTS employee_phone(
-	employee_ssn  VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS employee_phone (
+    employee_ssn VARCHAR(20) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    
-    PRIMARY KEY(phone),
-    FOREIGN KEY(employee_ssn) REFERENCES employee(ssn)
+    PRIMARY KEY (phone),
+    FOREIGN KEY (employee_ssn)
+        REFERENCES employee (ssn)
 );
 
-CREATE TABLE IF NOT EXISTS manager(
-	employee_ssn  VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS manager (
+    employee_ssn VARCHAR(20) NOT NULL,
     building_no INTEGER NOT NULL,
-    
-    PRIMARY KEY(employee_ssn),
-    FOREIGN KEY(employee_ssn) REFERENCES employee(ssn),
-    FOREIGN KEY(building_no) REFERENCES building(building_no)
+    PRIMARY KEY (employee_ssn),
+    FOREIGN KEY (employee_ssn)
+        REFERENCES employee (ssn),
+    FOREIGN KEY (building_no)
+        REFERENCES building (building_no)
 );
 
-CREATE TABLE IF NOT EXISTS worker(
-	employee_ssn CHAR(9) NOT NULL,
+CREATE TABLE IF NOT EXISTS worker (
+    employee_ssn CHAR(9) NOT NULL,
     building_no INTEGER NOT NULL,
-    type VARCHAR(20), 
-    
-    PRIMARY KEY(employee_ssn),
-    FOREIGN KEY(employee_ssn) REFERENCES employee(ssn),
-    FOREIGN KEY(building_no) REFERENCES building(building_no)
+    type VARCHAR(20),
+    PRIMARY KEY (employee_ssn),
+    FOREIGN KEY (employee_ssn)
+        REFERENCES employee (ssn),
+    FOREIGN KEY (building_no)
+        REFERENCES building (building_no)
 );
 
-CREATE TABLE IF NOT EXISTS person(
-	fname VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS person (
+    fname VARCHAR(20) NOT NULL,
     lname VARCHAR(20) NOT NULL,
     mis VARCHAR(20) NOT NULL,
     sex VARCHAR(20) NOT NULL,
     dob DATE NOT NULL,
     email VARCHAR(20) NOT NULL,
-	phone VARCHAR(20) NOT NULL,
-    
-    PRIMARY KEY(phone)
+    phone VARCHAR(20) NOT NULL,
+    PRIMARY KEY (phone)
 );
 
-CREATE TABLE IF NOT EXISTS tenant(
-	person_phone VARCHAR(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS tenant (
+    person_phone VARCHAR(20) NOT NULL,
     ssn CHAR(9) NOT NULL,
     depended_tenant_phone VARCHAR(20),
-    
-    PRIMARY KEY(person_phone),
-    FOREIGN KEY(person_phone) REFERENCES person(phone),
-    FOREIGN KEY(depended_tenant_phone) REFERENCES tenant(person_phone)
+    PRIMARY KEY (person_phone),
+    FOREIGN KEY (person_phone)
+        REFERENCES person (phone),
+    FOREIGN KEY (depended_tenant_phone)
+        REFERENCES tenant (person_phone)
 );
 
-CREATE TABLE IF NOT EXISTS non_tenant(
-	person_phone VARCHAR(20) NOT NULL,
-	address VARCHAR(500) NOT NULL,
-	
-    PRIMARY KEY(person_phone),
-    FOREIGN KEY(person_phone) REFERENCES person(phone)
+CREATE TABLE IF NOT EXISTS non_tenant (
+    person_phone VARCHAR(20) NOT NULL,
+    address VARCHAR(500) NOT NULL,
+    PRIMARY KEY (person_phone),
+    FOREIGN KEY (person_phone)
+        REFERENCES person (phone)
 );
 
-CREATE TABLE IF NOT EXISTS vehicle(
-	license_plate VARCHAR(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS vehicle (
+    license_plate VARCHAR(10) NOT NULL,
     type VARCHAR(20) NOT NULL,
     color VARCHAR(20) NOT NULL,
-    
-    PRIMARY KEY(license_plate)
+    PRIMARY KEY (license_plate)
 );
 
-CREATE TABLE IF NOT EXISTS tenant_vehicle(
-	vehicle_license_plate VARCHAR(10) NOT NULL,
-	register_date DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS tenant_vehicle (
+    vehicle_license_plate VARCHAR(10) NOT NULL,
+    register_date DATE NOT NULL,
     tenant_phone VARCHAR(20) NOT NULL,
-    
-    PRIMARY KEY(vehicle_license_plate),
-    FOREIGN KEY(vehicle_license_plate) REFERENCES vehicle(license_plate),
-    FOREIGN KEY(tenant_phone) REFERENCES tenant(person_phone)
+    PRIMARY KEY (vehicle_license_plate),
+    FOREIGN KEY (vehicle_license_plate)
+        REFERENCES vehicle (license_plate),
+    FOREIGN KEY (tenant_phone)
+        REFERENCES tenant (person_phone)
 );
 
-CREATE TABLE IF NOT EXISTS parking_slot(
-	parking_lot_no INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS parking_slot (
+    parking_lot_no INTEGER NOT NULL,
     section VARCHAR(5) NOT NULL,
     slot_no INTEGER NOT NULL,
     type VARCHAR(20) NOT NULL,
-    
     vehicle_license_plate VARCHAR(10),
     in_date DATE,
-    in_time time,
-	
-    PRIMARY KEY(parking_lot_no, section, slot_no),
-    FOREIGN KEY(parking_lot_no) REFERENCES parking_lot(parking_lot_no),
-    FOREIGN KEY(vehicle_license_plate) REFERENCES vehicle(license_plate)
+    in_time TIME,
+    PRIMARY KEY (parking_lot_no , section , slot_no),
+    FOREIGN KEY (parking_lot_no)
+        REFERENCES parking_lot (parking_lot_no),
+    FOREIGN KEY (vehicle_license_plate)
+        REFERENCES vehicle (license_plate)
 );
 
-CREATE TABLE IF NOT EXISTS service(
-	building_no INTEGER NOT NULL,
-	name VARCHAR(20) NOT NULL,
-    opentime time NOT NULL,
-    closetime time NOT NULL,
+CREATE TABLE IF NOT EXISTS service (
+    building_no INTEGER NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    opentime TIME NOT NULL,
+    closetime TIME NOT NULL,
     type VARCHAR(20) NOT NULL,
     operating_company VARCHAR(20) NOT NULL,
-    
-    PRIMARY KEY(building_no, name),
-    FOREIGN KEY(building_no) REFERENCES building(building_no)
+    PRIMARY KEY (building_no , name),
+    FOREIGN KEY (building_no)
+        REFERENCES building (building_no)
 );
 
-CREATE TABLE IF NOT EXISTS apartment(
-	building_no INTEGER NOT NULL, 
-    floor INTEGER NOT NULL, 
-    room_no INTEGER NOT NULL, 
-    no_of_bedroom INTEGER NOT NULL, 
-    no_of_bathroom INTEGER NOT NULL, 
-    type VARCHAR(20) NOT NULL, 
-    size FLOAT NOT NULL, 
-    
-    PRIMARY KEY(building_no, floor, room_no),
-	FOREIGN KEY(building_no) REFERENCES building(building_no)
+CREATE TABLE IF NOT EXISTS apartment (
+    building_no INTEGER NOT NULL,
+    floor INTEGER NOT NULL,
+    room_no INTEGER NOT NULL,
+    no_of_bedroom INTEGER NOT NULL,
+    no_of_bathroom INTEGER NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    size FLOAT NOT NULL,
+    PRIMARY KEY (building_no , floor , room_no),
+    FOREIGN KEY (building_no)
+        REFERENCES building (building_no)
 );
 
-CREATE TABLE IF NOT EXISTS rented_apartment(
-	building_no INTEGER NOT NULL, 
-    floor INTEGER NOT NULL, 
-    room_no INTEGER NOT NULL, 
-    lease_start_date DATE NOT NULL, 
-    lease_end_date DATE NOT NULL, 
-    lease_signed_date DATE NOT NULL, 
-    utilities_included BINARY NOT NULL, 
-    monthly_rate FLOAT NOT NULL, 
+CREATE TABLE IF NOT EXISTS rented_apartment (
+    building_no INTEGER NOT NULL,
+    floor INTEGER NOT NULL,
+    room_no INTEGER NOT NULL,
+    lease_start_date DATE NOT NULL,
+    lease_end_date DATE NOT NULL,
+    lease_signed_date DATE NOT NULL,
+    utilities_included BINARY NOT NULL,
+    monthly_rate FLOAT NOT NULL,
     lease_tenant_phone VARCHAR(20) NOT NULL,
-    
-	PRIMARY KEY(building_no, floor, room_no),
-	FOREIGN KEY(building_no) REFERENCES building(building_no),
-	FOREIGN KEY(lease_tenant_phone) REFERENCES tenant(person_phone)
+    PRIMARY KEY (building_no , floor , room_no),
+    FOREIGN KEY (building_no)
+        REFERENCES building (building_no),
+    FOREIGN KEY (lease_tenant_phone)
+        REFERENCES tenant (person_phone)
 );
 
-CREATE TABLE IF NOT EXISTS subscribed(
-	person_phone VARCHAR(20) NOT NULL,
-	service_building_no INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS subscribed (
+    person_phone VARCHAR(20) NOT NULL,
+    service_building_no INTEGER NOT NULL,
     service_name VARCHAR(20) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     type VARCHAR(20),
-    
-    PRIMARY KEY(person_phone, service_building_no, service_name),
-    FOREIGN KEY(person_phone) REFERENCES person(phone),
-    FOREIGN KEY(service_building_no, service_name) REFERENCES service(building_no, name)    
+    PRIMARY KEY (person_phone , service_building_no , service_name),
+    FOREIGN KEY (person_phone)
+        REFERENCES person (phone),
+    FOREIGN KEY (service_building_no , service_name)
+        REFERENCES service (building_no , name)
 );
 
 -- data population
